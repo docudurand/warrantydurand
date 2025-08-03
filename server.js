b3f00b19344ac16b36aa119f7f39bbabad154e46
docudurand <magvl4gleize@durandservices.fr>
2025-08-03 17:31:21 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -536,7 +536,9 @@ app.post("/api/admin/envoyer-fournisseur/:id", upload.fields([{ name: 'fichiers'
       <p>Bonjour,</p>
       <p>Vous trouverez ci-joint le dossier de garantie pour le produit&nbsp;: <strong>${dossier.produit_concerne || ''}</strong>.</p>
       ${adminMsg ? `<p>${adminMsg.replace(/\n/g,'<br>')}</p>` : ''}
-      </ul>
+	  <p style="margin-top:24px;font-weight:bold;">
+    Merci de répondre à l'adresse mail : <a href="mailto:${magasinEmail}" style="color:#004080;text-decoration:underline;">${magasinEmail}</a>
+  </p>
       <p>Cordialement,<br>L'équipe Garantie Durand Services</p>
     </div>`;
     await mailer.sendMail({
